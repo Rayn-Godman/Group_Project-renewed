ace2c2b62408adf15bed5acc8020d394da3334a3
Rayn Godman <me@rayn.com>
2020-09-29 19:41:29 +0200

    Add project files.

---
+++ Group_Project/HouseHold.vb
@@ -0,0 +1,128 @@
+﻿Option Explicit On
+Option Strict On
+Option Infer Off
+Public Class HouseHold
+    Implements IPerson
+
+    Private _Name As String ' Name of house hold owener
+    Private _Surname As String ' Surname of household owener
+    Private _Items() As Items
+    Private _NumItems As Integer
+    Private _HouseNumber As Integer
+    Private _NumberPeople As Integer  'to get number of people per household including owner
+    Private _PersonName As String ' to record name of Item owner
+    Private _PersonSurname As String ' to record Surname of item owener
+
+    'Constructor accepting three parameters and resizing Items array
+    Public Sub New(Name As String, surname As String, NumItems As Integer, numPeople As Integer)
+        _Name = Name
+        _Surname = surname
+        _NumberPeople = numPeople
+        _NumItems = ValidInt(NumItems) ' Number of items cannot be less than zero
+        ReDim _Items(_NumItems)
+
+        For i As Integer = 1 To NumItems
+            _Items(i) = New Items()
+        Next
+    End Sub
+
+    ' Declare properties for every attribute
+    Public ReadOnly Property Name As String
+        Get
+            Return _Name
+        End Get
+    End Property
+
+    Public ReadOnly Property Surname As String
+        Get
+            Return _Surname
+        End Get
+    End Property
+
+    Public Property Items(Index As Integer) As Items
+        Get
+            Return _Items(Index)
+        End Get
+        Set(value As Items)
+            _Items(Index) = value
+        End Set
+    End Property
+
+    Public Property HouseNumber As Integer
+        Get
+            Return _HouseNumber
+        End Get
+        Set(Value As Integer)
+            _HouseNumber = Value
+        End Set
+    End Property
+    Public ReadOnly Property NumItems As Integer
+        Get
+            Return _NumItems
+        End Get
+    End Property
+
+    Private Property IPerson_Name As String Implements IPerson.Name
+        Get
+            Return _PersonName
+        End Get
+        Set(value As String)
+            _PersonName = value
+        End Set
+    End Property
+
+    Private Property IPerson_Surname As String Implements IPerson.Surname
+        Get
+            Return _PersonSurname
+        End Get
+        Set(value As String)
+            _PersonSurname = value
+        End Set
+    End Property
+
+    Public Property NumberPeople As Integer
+        Get
+            Return _NumberPeople
+        End Get
+        Set(value As Integer)
+            _NumberPeople = value
+        End Set
+    End Property
+
+    ' Function to turn nagative integer to zero
+    Protected Function ValidInt(Value As Integer) As Integer
+        If (Value < 0) Then
+            Return 0
+        Else
+            Return Value
+        End If
+    End Function
+
+    'Overridable function that will calculate average of recycable and waste items
+    Public Function CalcAve() As Double
+        Dim total As Double = 0
+        total += _NumItems
+        Dim ave As Double
+        ave = total / _NumberPeople
+        Return ave
+    End Function
+
+    'Function that will return string based on each average of recycable and non recycable items 
+    Public Function CalcRating() As String
+        Dim rating As String = ""
+        Select Case CalcAve()
+            Case Is <= 5
+                rating = "Good."
+            Case Is > 5
+                rating = "Okay"
+            Case Is >= 10
+                rating = "Bad"
+        End Select
+        Return rating
+    End Function
+
+    Public Function Display() As String
+        Return "Owner Name & Surname: " & _Name & " " & _Surname & Environment.NewLine & "Number Of People in the House: " & CStr(_NumberPeople) & "House Number: " &
+            CStr(_HouseNumber) & Environment.NewLine & "Rating: " & CalcRating()
+    End Function
+End Class
---
+++ Group_Project/Items.vb
@@ -0,0 +1,90 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+Public Class Items
+    'Declare instance variables
+    Private _ItemName As String
+    Private _MaterialType As String
+    Private _SerialNum As Integer
+    Private _IsReusable As Boolean
+    Private _IsBiodigradable As Boolean
+
+
+    'Declare all property methods for each of the instance variables
+    Public Property ItemName As String
+        Get
+            Return _ItemName
+        End Get
+        Set(value As String)
+            _ItemName = value
+        End Set
+    End Property
+
+    Public Property Material As String
+        Get
+            Return _MaterialType
+        End Get
+        Set(value As String)
+            _MaterialType = value
+        End Set
+    End Property
+
+    Public ReadOnly Property SerialNum As Integer
+        Get
+            Return _SerialNum
+        End Get
+    End Property
+
+    Public Property IsReusable As Boolean
+        Get
+            Return _IsReusable
+        End Get
+        Set(value As Boolean)
+            _IsReusable = value
+        End Set
+    End Property
+
+    Public Property IsBiodigradable As Boolean
+        Get
+            Return _IsBiodigradable
+        End Get
+        Set(value As Boolean)
+            _IsBiodigradable = value
+        End Set
+    End Property
+
+    'Function to validate any property methods
+    Protected Function ValidInt(num As Integer) As Integer
+        If num < 0 Then
+            Return 0
+        Else
+            Return num
+        End If
+    End Function
+
+    'Function that return string of details of each item displayed
+    Public Overridable Function Display() As String
+        Return "Item Name: " & ItemName & "Type of material: " & "Made of " & Material & Environment.NewLine
+    End Function
+
+    ' This function to warn user about harm of item if it cannot be Disposed in other way
+    Public Overridable Function Warning() As String 'This form of function should be in nonReclable class
+        Select Case IsBiodigradable
+            Case True
+                If (IsReusable = True) Then
+                    Return Display() & " / NB!!! Should be Reused/Donated to Avoid Dumbing"
+                Else
+                    Return Display() & " / It is less harmful to environment"
+                End If
+            Case False
+                If (IsReusable = True) Then
+                    Return Display() & " / NB!!! Should be Reused/Donated to Avoid Dumbing"
+                Else
+                    Return Display() & " / NB!!! It is very Harmful to the Environment Dumbing Should be limited"
+                End If
+            Case Else
+                Return Nothing
+        End Select
+    End Function
+End Class
